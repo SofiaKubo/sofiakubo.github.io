71bb0e9454c4815f49241742def1e036cfdcc464
SofiaKubo <skuboreva@gmail.com>
2025-12-05 17:03:30 +0100

    feat: update intro text in App component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -7,8 +7,20 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Текст писать тут
+        <p className="app-text">
+          Привет! Меня зовут Соня. У меня уже есть опыт работы с HTML, CSS, JavaScript и Git. Раньше я изучала Java, а
+          сейчас хочу углубиться в React, современный фронтенд-стек и работу с API. Мои цели на курсе — научиться
+          профессионально разрабатывать веб-приложения, понимать архитектуру React-приложений, применять TypeScript на
+          практике и работать с реальными проектами.
+          <br />
+          <br />
+          В дальнейшем я хочу совмещать фронтенд и бэкенд-разработку, создавая полноценные веб-приложения.
+          <br />
+          <br />
+          Мой GitHub:{' '}
+          <a href="https://github.com/SofiaKubo" target="_blank" rel="noreferrer">
+            github.com/SofiaKubo
+          </a>
         </p>
       </header>
     </div>
