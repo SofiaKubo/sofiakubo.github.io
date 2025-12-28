e86a3481e92f050fb5064887ee2fffd7c7c3d902
SofiaKubo <skuboreva@gmail.com>
2025-12-28 18:29:53 +0100

    feat: add header component with sticky layout

---
+++ src/shared/header/Header.stories.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import type { Meta, StoryObj } from '@storybook/react';
+import Header from './Header';
+
+const meta: Meta<typeof Header> = {
+  title: 'shared/Header',
+  component: Header,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Header>;
+
+export const Default: Story = {
+  render: () => <Header />,
+};
